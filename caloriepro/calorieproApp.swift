97fc3f173b5c91729b15154328eda3b1044ed5e2
Mufli-Mohideen <mufli2mail@gmail.com>
2024-08-13 10:32:58 +0530

    Initial Commit

---
+++ caloriepro/calorieproApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorieproApp.swift
+//  caloriepro
+//
+//  Created by Mufli Mohideen on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct calorieproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
