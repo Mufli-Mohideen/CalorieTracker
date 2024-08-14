6d4e951d5b9452350acaa7629262643d867c829e
Mufli-Mohideen <mufli2mail@gmail.com>
2024-08-14 11:19:05 +0530

    first commit

--- caloriepro/ContentView.swift
+++ caloriepro/ContentView.swift
@@ -1,24 +1,61 @@
-//
-//  ContentView.swift
-//  caloriepro
-//
-//  Created by Mufli Mohideen on 2024-08-13.
-//
-
 import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Hello")
+                    .foregroundColor(.black)
+                    .font(.custom("Avenir Next Medium", size: 28))
+                    .offset(y:-10)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 10)
+                
+                Text("MUFLI!")
+                    .foregroundColor(.black)
+                    .font(.custom("Avenir Next Condensed Bold", size: 28))
+                    .offset(y:-10)
+                    .fontWeight(.bold)
+                
+                Spacer()
+            }
+            .frame(height: 45)
+            .padding(.horizontal)
+            .background(Color(hex: "#ADFF2F"))
+            Spacer()
         }
-        .padding()
+        .background(Color.black.ignoresSafeArea())
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
